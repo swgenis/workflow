e47653dadeb7154976617a7164b2f0b036886b8d
swgenis <sw@opencollab.co.za>
2015-04-20 10:32:52 +0200

    Implement arquillian test framework

--- backbone/backbone-services/src/test/java/za/ac/nwu/workflow/backbone/person/service/PersonServiceTest.java
+++ backbone/backbone-services/src/test/java/za/ac/nwu/workflow/backbone/person/service/PersonServiceTest.java
@@ -2,9 +2,16 @@ package za.ac.nwu.workflow.backbone.person.service;
 
 import javax.inject.Inject;
 
+import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import za.ac.nwu.workflow.backbone.person.service.impl.PersonServiceMapImpl;
 
 @RunWith(Arquillian.class)
 public class PersonServiceTest {
@@ -12,14 +19,29 @@ public class PersonServiceTest {
 	@Inject
 	private PersonService personService;
 	
+	@Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+            .addClass(PersonServiceMapImpl.class)
+            .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+	
 	@Test
 	public void testGetLeaveApplicationById(){
 		Person person = personService.getPersonById("bob");
+		Assert.assertNotNull(person);
 	}
 	
 	@Test
-	public void testInsertPerson(){
+	public void testInsertPerson() throws Exception{
+		Person person = new Person();
+		person.setId("test");
+		person.setName("Test");
+		personService.insertPerson(person);
 		
+		Person retrievedPerson = personService.getPersonById("test");
+		Assert.assertNotNull(retrievedPerson);
+		Assert.assertEquals("Test", retrievedPerson.getName());
 	}
 	
 	@Test
--- backbone/backbone-services/src/test/java/za/ac/nwu/workflow/backbone/type/service/TypeServiceTest.java
+++ backbone/backbone-services/src/test/java/za/ac/nwu/workflow/backbone/type/service/TypeServiceTest.java
@@ -2,16 +2,34 @@ package za.ac.nwu.workflow.backbone.type.service;
 
 import javax.inject.Inject;
 
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import za.ac.nwu.workflow.backbone.type.service.impl.TypeServiceMapImpl;
+
+@RunWith(Arquillian.class)
 public class TypeServiceTest {
 	
 	@Inject
 	private TypeService typeService;
 	
+	@Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+            .addClass(TypeServiceMapImpl.class)
+            .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+	
 	@Test
 	public void testGetTypeById(){
 		Type type = typeService.getTypeByKey("type.1");
+		Assert.assertNotNull(type);
 	}
 	
 	@Test
