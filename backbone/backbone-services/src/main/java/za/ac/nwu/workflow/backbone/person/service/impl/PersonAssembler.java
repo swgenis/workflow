373da088d4886b1caf38c532e791d3c4016effaa
unknown <13098241@P13098241.adt.nwu.ac.za>
2015-05-06 09:38:12 +0200

    WOR-6 VSSKim impl of PersonService

---
+++ backbone/backbone-services/src/main/java/za/ac/nwu/workflow/backbone/person/service/impl/PersonAssembler.java
@@ -0,0 +1,18 @@
+package za.ac.nwu.workflow.backbone.person.service.impl;
+
+import org.kuali.rice.kim.api.identity.entity.EntityDefault;
+
+import za.ac.nwu.workflow.backbone.person.Person;
+
+public class PersonAssembler {
+    
+    public Person assemblePerson(EntityDefault kimEntity) {
+	Person person = new Person();
+	person.setId(kimEntity.getEntityId());
+	person.setName(kimEntity.getName().getFirstNameUnmasked());
+	person.setSurname(kimEntity.getName().getLastNameUnmasked());
+	person.setEmail(kimEntity.getEntityTypeContactInfos().get(0).getDefaultEmailAddress().getEmailAddressUnmasked());
+	return person;
+    }
+
+}
---
+++ backbone/backbone-services/src/main/java/za/ac/nwu/workflow/backbone/producers/ConfigurationPropertyProducer.java
@@ -0,0 +1,45 @@
+package za.ac.nwu.workflow.backbone.producers;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.inject.Singleton;
+
+@Singleton
+public class ConfigurationPropertyProducer {
+
+    private Properties properties;
+
+    @PostConstruct
+    private void initializeProperties() throws IOException {
+	InputStream inputStream = getClass().getClassLoader().getResourceAsStream("application.properties");
+	properties = new Properties();
+
+	if (inputStream != null) {
+	    properties.load(inputStream);
+	}
+
+    }
+
+    @Produces
+    @ConfigurationProperty
+    private String produceStringProperty(InjectionPoint injectionPoint) throws IOException {
+	String key = injectionPoint.getAnnotated().getAnnotation(ConfigurationProperty.class).value();
+	String defaultValue = injectionPoint.getAnnotated().getAnnotation(ConfigurationProperty.class).defaultValue();
+
+	return properties.getProperty(key, defaultValue);
+    }
+
+    @Produces
+    @ConfigurationProperty
+    private boolean produceBooleanProperty(InjectionPoint injectionPoint) throws IOException {
+	String key = injectionPoint.getAnnotated().getAnnotation(ConfigurationProperty.class).value();
+	String defaultValue = injectionPoint.getAnnotated().getAnnotation(ConfigurationProperty.class).defaultValue();
+
+	return Boolean.valueOf(properties.getProperty(key, defaultValue).toLowerCase());
+    }
+}
