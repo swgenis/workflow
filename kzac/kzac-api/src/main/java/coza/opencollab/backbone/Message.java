2c35f6f2cc247da69e4aa48c0df1ef20835c354c
swgenis <sw@opencollab.co.za>
2015-06-20 16:12:21 +0200

    Create initial external api

---
+++ kzac/kzac-api/src/main/java/coza/opencollab/backbone/Message.java
@@ -0,0 +1,24 @@
+package coza.opencollab.backbone;
+
+/**
+ * 
+ * @author SW Genis
+ *
+ */
+public class Message {
+
+    private String msg;
+
+    public Message(String msg) {
+	this.msg = msg;
+    }
+
+    public String getMsg() {
+	return msg;
+    }
+
+    public void setMsg(String msg) {
+	this.msg = msg;
+    }
+
+}
